d82536a623b4fdf80e849bf0121e37744a27519f
timdams <dams.tim@telenet.Be>
2017-05-08 13:43:12 +0200

    Gaat goed he

--- src/SendToCalibre_OutlookPlugin/ThisAddIn.cs
+++ src/SendToCalibre_OutlookPlugin/ThisAddIn.cs
@@ -5,13 +5,19 @@ using System.Text;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
 
 namespace SendToCalibre_OutlookPlugin
 {
+    //Source ? https://msdn.microsoft.com/en-us/library/ms268754.aspx
     public partial class ThisAddIn
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            
+
+            //Save attachments https://msdn.microsoft.com/en-us/library/ms268754.aspx
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
@@ -19,6 +25,13 @@ namespace SendToCalibre_OutlookPlugin
             // Note: Outlook no longer raises this event. If you have code that 
             //    must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
         }
+
+        protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return Globals.Factory.GetRibbonFactory().CreateRibbonManager(
+                         new Microsoft.Office.Tools.Ribbon.IRibbonExtension[] { new Ribbon1() });
+        }
+
 
         #region VSTO generated code
 
